f85a2c633a3ff2c29aea7f5e3c1ebd7c1cf8b192
vadim <sergey.serysev@gmail.com>
2026-03-27 21:26:21 +0300

    lr1: modify test_project to print 'Hello, Dart! ves 1 punkt'

--- lr1/test_project/bin/test_project.dart
+++ lr1/test_project/bin/test_project.dart
@@ -1,5 +1,3 @@
-import 'package:test_project/test_project.dart' as test_project;
-
 void main(List<String> arguments) {
-  print('Hello world: ${test_project.calculate()}!');
+  print('Hello, Dart!');
 }
