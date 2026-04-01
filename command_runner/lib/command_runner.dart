6e581dd43e25b9a727867c82c4422546a4bf988c
vadim <sergey.serysev@gmail.com>
2026-04-01 15:57:58 +0300

    Lr4: добавлен класс CommandRunner в пакет command_runner

--- command_runner/lib/command_runner.dart
+++ command_runner/lib/command_runner.dart
@@ -1,8 +1,7 @@
-/// Support for doing something awesome.
+/// A simple command runner to handle command-line arguments.
 ///
-/// More dartdocs go here.
+/// More extensive documentation for this library goes here.
 library;
 
 export 'src/command_runner_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+// TODO: Export any other libraries intended for clients of this package.
