4300b5aa8af8a11f1ceba38dd7b32a3ef114eeed
vadim <sergey.serysev@gmail.com>
2026-04-01 12:25:14 +0300

    Комментарий: реализована команда search с запросом названия статьи и выводом имитации поиска

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 const version = '0.0.1';
 
 void printUsage() {
@@ -11,7 +12,26 @@ void main(List<String> arguments) {
     printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
+   } else if (arguments.first == 'search') {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
   } else {
     printUsage();
   }
 }
+
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Looking up articles about "$articleTitle". Please wait.');
+  print('Here ya go!');
+  print('(Pretend this is an article about "$articleTitle")');
+}
+
