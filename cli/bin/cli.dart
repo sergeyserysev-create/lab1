5bc7b10365f45f33821708a1a065b3722ee286f9
vadim <sergey.serysev@gmail.com>
2026-04-01 16:24:10 +0300

    Lr4: исправлен синтаксис cli.dart, удалены неиспользуемые импорты

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,7 +1,6 @@
-import 'dart:io';
-import 'package:http/http.dart' as http;
 import 'package:command_runner/command_runner.dart';
 
-void main(List<String> arguments) async { // main is now async and awaits the runner
-  var runner = CommandRunner(); // Create an instance of your new CommandRunner
-  await runner.run(arguments); // Call its run method, awaiting its Future<void>
+void main(List<String> arguments) async {
+  var runner = CommandRunner();
+  await runner.run(arguments);
+}
